ab64eaf64f6440197d3826e48b31c2312962c669
shivtwr <mail.shivtiwari@gmail.com>
2026-06-04 13:34:14 +0530

    sign-up otp fix

--- app/api/auth/send-otp/route.ts
+++ app/api/auth/send-otp/route.ts
@@ -4,18 +4,19 @@ import resend from "@/lib/resend";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const { email, firstName } = await req.json();
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    await redis.set(`otp:${email}`, otp, `EX`, 300);
-    try {
-        const resendResponse = await resend.emails.send({
-            from: "CipherShare <noreply@ciphershare.in>",
-            to: email,
-            subject: "Email Verification OTP",
-            react: OtpEmail({ firstName, otp }),
-        });
-        return NextResponse.json({ success: true }, { status: 200 });
-    } catch (e) {
-        return NextResponse.json(e, { status: 500 });
-    }
+  const { email, firstName } = await req.json();
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  await redis.set(`otp:${email}`, otp, `EX`, 300);
+  try {
+    const resendResponse = await resend.emails.send({
+      from: "CipherShare <noreply@ciphershare.s17.in>",
+      to: email,
+      subject: "Email Verification OTP",
+      react: OtpEmail({ firstName, otp }),
+    });
+    console.log(resendResponse);
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(e, { status: 500 });
+  }
 }
--- lib/redis.ts
+++ lib/redis.ts
@@ -1,10 +1,10 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-    host: "redis-13490.crce206.ap-south-1-1.ec2.redns.redis-cloud.com",
-    port: 13490,
-    username: "default",
-    password: process.env.REDIS_PASSWORD,
+  host: "redis-14517.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  port: 14517,
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
 });
 
 export default redis;
