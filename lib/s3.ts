01258660b0422d4d2601f03bfaa890730515db2a
shivtwr <mail.shivtiwari@gmail.com>
2026-06-08 22:07:24 +0530

    AWS S3 stuff fixed

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,36 +1,36 @@
 import {
-    S3Client,
-    GetObjectCommand,
-    PutObjectCommand,
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({
-    region: "ap-south-1",
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-    },
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+  },
 });
 
-const bucket = "ciphershare";
+const bucket = "cipher-share";
 
 export async function putObjectUrl() {
-    const key = `uploads/files/file-${Date.now()}.json`;
-    const command = new PutObjectCommand({
-        Bucket: bucket,
-        Key: key,
-        ContentType: "application/json",
-    });
-    const url = await getSignedUrl(s3Client, command);
-    return { url, key };
+  const key = `uploads/files/file-${Date.now()}.json`;
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    ContentType: "application/json",
+  });
+  const url = await getSignedUrl(s3Client, command);
+  return { url, key };
 }
 
 export async function getObjectUrl(key: string) {
-    const command = new GetObjectCommand({
-        Bucket: bucket,
-        Key: key,
-    });
-    const url = await getSignedUrl(s3Client, command);
-    return url;
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+  const url = await getSignedUrl(s3Client, command);
+  return url;
 }
